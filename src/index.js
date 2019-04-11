fcd19c84412db2fb735095e9f2fc6cc4452fb32d
Justin Baltazar <justin.t.baltazar.21@dartmouth.edu>
2019-04-11 00:02:25 -0400

    finished up to imagefile loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hello world!");
+import $ from 'jquery';
+import './style.scss';
+
+let time = 0;
+setInterval(() => {
+  time += 1;
+  $('#main').html(`You have been on this page for ${time} seconds.`);
+}, 1000);
